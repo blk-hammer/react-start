3280b199f73c0df432f319186e68a9caa4626948
Rishabh Agnihotri <rishabhagnihotri1999@gmail.com>
2020-05-21 22:28:42 +0530

    fixed redundant api calls issue

--- file-5/src/actions/index.js
+++ file-5/src/actions/index.js
@@ -20,3 +20,33 @@ export const fetchUser = (id) => async (dispatch) => {
     payload: response.data,
   });
 };
+
+// // method 1: using private function
+// //         disadvantage: if any change is made to user it wont refactor it into state
+// export const fetchUser = (id) => dispatch=> _fetchUser(id,dispatch);
+
+// const _fetchUser=_.memoize(async (id,dispatch) => {
+
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({
+//     type: "FETCH_USER",
+//     payload: response.data,
+//   });
+
+// });
+
+// method 2: using parent function
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  // // alternate lodash chain statement below
+  // const userIds = _.uniq(_.map(getState().posts, "userId"));
+  // userIds.forEach((id) => dispatch(fetchUser(id)));
+
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value();
+};
